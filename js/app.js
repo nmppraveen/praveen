076db14da5840d221ea9308cc83599ecf7aa1029
nmppraveen <nmppraveen@gmail.com>
2020-10-12 23:07:44 +0530

    My Assignment1.2

---
+++ js/app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+
+var storeApp = angular.module('AngularStore', []).
+  config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+      when('/store', {
+        templateUrl: 'partials/store.htm',
+        controller: storeController 
+      }).
+      when('/products/:productSku', {
+        templateUrl: 'partials/product.htm',
+        controller: storeController
+      }).
+      when('/cart', {
+        templateUrl: 'partials/shoppingCart.htm',
+        controller: storeController
+      }).
+      otherwise({
+        redirectTo: '/store'
+      });
+}]);
+
+storeApp.factory("DataService", function () {
+
+   
+    var myStore = new store();
+
+ 
+    var myCart = new shoppingCart("AngularStore");
+	
+    myCart.addCheckoutParameters("PayPal", "bernardo.castilho-facilitator@gmail.com");
+
+    myCart.addCheckoutParameters("Google", "500640663394527",
+        {
+            ship_method_name_1: "UPS Next Day Air",
+            ship_method_price_1: "20.00",
+            ship_method_currency_1: "USD",
+            ship_method_name_2: "UPS Ground",
+            ship_method_price_2: "15.00",
+            ship_method_currency_2: "USD"
+        }
+    );
+
+    return {
+        store: myStore,
+        cart: myCart
+    };
+});
---
+++ js/controller.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+
+function storeController($scope, $routeParams, DataService) {
+
+    $scope.store = DataService.store;
+    $scope.cart = DataService.cart;
+
+    $scope.cart.itemsChanged = function (e) {
+        if (!$scope.$$phase) {
+            $scope.$apply();
+        }
+    }
+
+
+    if ($routeParams.productSku != null) {
+        $scope.product = $scope.store.getProduct($routeParams.productSku);
+    }
+}
